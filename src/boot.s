712f049359458d47e3da48aed5fa82c441fac6e9
suwa <suwuako@protonmail.com>
2024-09-08 17:01:33 +1000

    do some tests i should probably switch branches

--- src/boot.s
+++ src/boot.s
@@ -1,29 +1,28 @@
 [org 0x7c00]
 [bits 16]
 
-start:
-    cli
-    mov ax, 0x00
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7c00
-    sti
-    mov si, msg
+mov ah, 0x0e
+mov bx, msg
+mov cl, 0
 
 print:
-    lodsb
-    cmp al, 0
-    je done
-    mov ah, 0x0E
+    mov al, [bx]
     int 0x10
+    inc bx
+
+    cmp al, cl
+    je read_disk
     jmp print
+
+read_disk:
+    jmp done
 
 done:
     cli
     hlt
 
-msg: db "Hello world!", 0
+msg: 
+    db "Hello world?", 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
